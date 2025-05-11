05432038e40415d34482ad63ea2aa48cc84ea54d
Flayaaa <Flayaaa@outlook.com>
2025-05-11 22:31:59 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
+radio.setGroup(173);
 
+basic.forever(function () {
+        let x = Math.round(input.acceleration(Dimension.X));
+        let y = Math.round(input.acceleration(Dimension.Y));
+
+        console.log(`X: ${x}, Y: ${y}`);
+
+        radio.sendValue("speedX", x);
+        radio.sendValue("speedY", y);
+
+input.onButtonPressed(Button.B, function () { 
+    radio.sendValue("Speed0", 0);
+})
+    
+});
