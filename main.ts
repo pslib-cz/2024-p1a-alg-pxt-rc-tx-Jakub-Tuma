843c37e0f37183a0799ce4f4830a00802c40ac61
Flayaaa <Flayaaa@outlook.com>
2025-05-12 09:55:20 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,21 @@
 radio.setGroup(173);
+radio.setTransmitPower(7)
+let go = true
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    go = !go
+    if (go) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
 
 basic.forever(function () {
-        let x = Math.round(input.acceleration(Dimension.X));
-        let y = Math.round(input.acceleration(Dimension.Y));
+    let x = Math.round(input.acceleration(Dimension.X));
+    let y = Math.round(input.acceleration(Dimension.Y));
 
-        console.log(`X: ${x}, Y: ${y}`);
+    console.log(`X: ${x}, Y: ${y}`);
 
-        radio.sendValue("speedX", x);
-        radio.sendValue("speedY", y);
-
-input.onButtonPressed(Button.B, function () { 
-    radio.sendValue("Speed0", 0);
-})
-    
+    radio.sendValue("SpeedX", x) && radio.sendValue("SpeedY", y);
 });
