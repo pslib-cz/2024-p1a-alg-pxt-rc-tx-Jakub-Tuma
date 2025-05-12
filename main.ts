6769b1f32d38ee62d740621c9aca6407c27ea343
Flayaaa <Flayaaa@outlook.com>
2025-05-12 09:52:40 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
+radio.setGroup(173);
+radio.setTransmitPower(7)
+let go = true
 
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    go = !go
+    if (go) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+
+basic.forever(function () {
+    let x = Math.round(input.acceleration(Dimension.X));
+    let y = Math.round(input.acceleration(Dimension.Y));
+
+    console.log(`X: ${x}, Y: ${y}`);
+
+    radio.sendValue("SpeedX", x) && radio.sendValue("SpeedY", y);
+});
