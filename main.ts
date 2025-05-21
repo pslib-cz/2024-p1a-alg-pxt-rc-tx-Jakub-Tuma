0e7745639e5e0f7ba8479ce5e3712642e0b4a3eb
Flayaaa <Flayaaa@outlook.com>
2025-05-21 19:01:47 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 radio.setGroup(54)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(4)
+radio.setTransmitSerialNumber(true)
+let go = true
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    go = !go
+    if (go) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+        radio.sendString("Stop")
+    }
 
 basic.forever(function () {
-    let x = input.acceleration(Dimension.X)
-    let y = input.acceleration(Dimension.Y)
+    const ab = Math.round(input.acceleration(Dimension.X)); + Math.round(input.acceleration(Dimension.Y));
 
-    radio.sendString(`${x},${y}`)
+    console.log(`${ab}`)
 
+    radio.sendString(`${ab}`)
     basic.pause(100)
-})
-
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("Stop")
-})
-
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
-
-})
+})})
