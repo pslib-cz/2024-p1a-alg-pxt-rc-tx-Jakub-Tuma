c6436e1bf1368e052d7df05b714e757f1d4528d7
Flayaaa <Flayaaa@outlook.com>
2025-06-03 20:42:29 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,10 +13,10 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     }
 })
 basic.forever(function () {
-    const xd = Math.round(input.acceleration(Dimension.X));
-    const yd = Math.round(input.acceleration(Dimension.Y));
-    console.log(`${xd, yd}`)
-    radio.sendString(`${xd, yd}`)
+    const x = Math.round(input.acceleration(Dimension.X));
+    const y = Math.round(input.acceleration(Dimension.Y));
+    console.log(`${x, y}`)
+    radio.sendString(`${x, y}`)
     basic.pause(60)
 })
 
