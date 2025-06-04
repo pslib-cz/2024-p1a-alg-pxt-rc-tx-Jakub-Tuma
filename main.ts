0c3dfe15032690a5cafe84272cc061bfc16540ad
Flayaaa <Flayaaa@outlook.com>
2025-06-04 12:13:26 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,20 +2,13 @@ radio.setGroup(54)
 radio.setTransmitPower(7)
 radio.setFrequencyBand(4)
 radio.setTransmitSerialNumber(true)
-let go = true
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    go = !go
-    if (go) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
-        radio.sendString(`${"Stop"}`)
-    }
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(`${"Stop"}`)
 })
+
 basic.forever(function () {
     const x = Math.round(input.acceleration(Dimension.X));
     const y = Math.round(input.acceleration(Dimension.Y));
-    console.log(`${x, y}`)
     radio.sendString(`${x, y}`)
     basic.pause(60)
 })
